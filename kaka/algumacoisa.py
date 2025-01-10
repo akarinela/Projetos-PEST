409b02e23e7a921202c0e7c1a02ec7b1699f4c9d
akarinera <ana.karine06@aluno.ifce.edu.br>
2025-01-10 11:18:59 -0300

    Add files via upload

---
+++ kaka/algumacoisa.py
@@ -0,0 +1,6 @@
+L = ['c', 1, 3.5, 'uva', True]
+
+tamanho = len(L)
+
+for i in range(tamanho):
+    print(L[i])
---
+++ kaka/teste1.2.py
@@ -0,0 +1,9 @@
+n1 = float(input('Digite um nota: '))
+n2 = float(input('Digite um nota: '))
+n3 = float(input('Digite um nota: '))
+n4 = float(input('Digite um nota: '))
+n5 = float(input('Digite um nota: '))
+n6 = float(input('Digite um nota: '))
+n7 = float(input('Digite um nota: '))
+
+notas = 
