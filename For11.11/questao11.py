d30d1832ccdc18da8af14f90cb7567614c880bd8
ana karine <ana.karine06@aluno.ifce.edu.br>
2025-05-14 10:52:32 -0300

    Delete For11.11 directory

--- For11.11/questao11.py
+++
@@ -1,7 +0,0 @@
-# Crie um código usando o loop for para mostrar todos os número ímpares de 100 até 0
-
-for a in range(101):
-    if a %2 == 1:
-        print('O número é ímpar')
-    else:
-        print('O núnero não é ímpar')
