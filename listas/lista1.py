690dcb954b31e2892c53e3adf500b5f860db6f6a
ana karine <ana.karine06@aluno.ifce.edu.br>
2025-05-14 13:44:21 -0300

    Delete listas directory

--- listas/lista1.py
+++
@@ -1,9 +0,0 @@
-lista = []
-
-while True:
-    numero = int(input("digite um numero(digite 0 para sair):"))
-    if numero != 0:
-        lista.append(numero)
-    else:
-        break
-print(lista)
